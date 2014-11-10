a8c849c3d63e8c57fb662d7fb54b350882c4e907
Cody Mortimer <cmm7393@g.ri.tedu>
2014-11-09 20:29:29 -0500

    Version 0.02
    Basic keyboard control, will try switching to gamepad later tonight

---
+++ Assets/CharacterController2d.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController2d : MonoBehaviour {
+	int speed = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		getInput();
+	}
+	
+	void getInput(){
+		Vector2 newPos = transform.position;
+		Quaternion newRot = transform.rotation;
+		if(Input.GetKey("w")){
+			newPos.y += speed * Time.deltaTime;
+		}
+		
+		else if(Input.GetKey("s")){
+			newPos.y -= speed * Time.deltaTime;
+		}
+		
+		if(Input.GetKey("a")){
+			newPos.x -= speed * Time.deltaTime;
+		}
+		
+		else if(Input.GetKey("d")){
+			newPos.x += speed * Time.deltaTime;
+		}
+		transform.position = newPos;
+	}
+}
